26fcebd12146aaeec01117265f918a15c6b802bb
Jolei <djoleife@gmail.com>
2025-03-07 10:46:50 +0800

    ADD display absolute value in Pie Chart

--- data_analysis.py
+++ data_analysis.py
@@ -8,11 +8,19 @@ df = dm.import_csv("sample_data.csv")
 #total amount per category
 total_by_category = df.groupby("Category")["Amount"].sum()
 
+def absolute_value(val):
+    total = sum(total_by_category)
+    absolute = int(val/100 * total)
+    return f"{absolute}"
+
 ax = total_by_category.plot(kind = 'pie',
-                      autopct="%1.1f%%",
+                      autopct= absolute_value,
                       startangle=90,
                       ylabel="",
                       legend=False)
 ax.set_title("Spending by Category")
 print(total_by_category)
 plt.show()
+
+
+
