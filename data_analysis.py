faf0f0f5bc28a3687b4c50d7e82f6eb0da792077
Jolei <djoleife@gmail.com>
2025-03-07 12:57:08 +0800

    [FIX] edit code function for total by category

--- data_analysis.py
+++ data_analysis.py
@@ -6,21 +6,26 @@ csv_file_name = "sample_data.csv"
 df = dm.import_csv("sample_data.csv")
 
 #total amount per category
-total_by_category = df.groupby("Category")["Amount"].sum()
+def spending_total_by_category(df):
+    if 'Category' not in df.columns or 'Amount' not in df.columns:
+        return
 
-def absolute_value(val):
-    total = sum(total_by_category)
-    absolute = int(val/100 * total)
-    return f"{absolute}"
+    total_by_category = df.groupby("Category")["Amount"].sum()
 
-ax = total_by_category.plot(kind = 'pie',
+
+    def absolute_value(val):
+        total = sum(total_by_category)
+        absolute = int(val/100 * total)
+        return f"{absolute}"
+
+    ax = total_by_category.plot(kind = 'pie',
                       autopct= absolute_value,
                       startangle=90,
                       ylabel="",
                       legend=False)
-ax.set_title("Spending by Category")
-print(total_by_category)
-plt.show()
+    ax.set_title("Spending by Category")
+    print(total_by_category)
+    plt.show()
 
 
 
