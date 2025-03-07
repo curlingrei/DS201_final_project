881047e27ad920dc222b614ef31f1ff657193090
Jolei <djoleife@gmail.com>
2025-03-07 11:18:27 +0800

    [FEATURE] add calculation of average monthly spending

--- data_analysis.py
+++ data_analysis.py
@@ -1,12 +1,15 @@
 import data_management as dm
 import matplotlib.pyplot as plt
 import pandas as pd
+import calendar
 
 csv_file_name = "sample_data.csv"
 df = dm.import_csv("sample_data.csv")
 
 #total amount per category
 total_by_category = df.groupby("Category")["Amount"].sum()
+
+# def choose_display(dis)
 
 def absolute_value(val):
     total = sum(total_by_category)
@@ -21,6 +24,19 @@ ax = total_by_category.plot(kind = 'pie',
 ax.set_title("Spending by Category")
 print(total_by_category)
 plt.show()
+
+
+#Calculate Average Monthly Spending
+df['Date'] = pd.to_datetime(df['Date'])
+df['Month'] = df['Date'].dt.month
+df['month_name'] = df['Month'].apply(lambda x: calendar.month_name[x])
+
+monthly_spending = df.groupby("month_name")["Amount"].sum()
+print(monthly_spending)
+
+average_monthly_spending = monthly_spending.mean()
+print(f"Your average monthly spending is {average_monthly_spending:.2f}")
+
 
 
 
